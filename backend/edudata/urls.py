5c1bc12099a0cc04dbc527460019d9953ebfddf5
Amange1di <amangeldi2702006@gmail.com>
2026-05-10 14:27:44 +0600

    Fix-urls

--- backend/edudata/urls.py
+++ backend/edudata/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import path as django_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,5 +13,11 @@ urlpatterns = [
     path("api/reports/", include("reports.urls")),
 ]
 
+# Media files
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Serve media files in production
+    urlpatterns += [
+        django_path(r'^media/(?P<path>.*), serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
